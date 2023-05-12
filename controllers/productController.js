64d6a57b28a2d1cda1cdb2f8fc3336b9368f4e3b
unknown <kaah6978@gmail.com>
2023-05-12 18:02:06 +0300

    test

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,61 +1,57 @@
 import Product from "../models/productModel.js";
 
+export const createProduct = async (req, res) => {
+  try {
+    const { name, category, image, description, price, countInStock } =
+      req.body;
 
-export const createProduct = async(req, res) =>{
-    const {name, category, image, description, price, countInStock} = req.body;
+      let product = new Product({
+      name,
+      category,
+      image,
+      description,
+      price,
+      countInStock,
+    });
 
-    const product = await Product.create(
-        {
-            name, category, image, description, price, countInStock
-        }
-    );
-
-    if (product) {
-        res.status(201).json(product)
-        
-    }
-    else{
-        res.status(500).json({message: 'Product Not Created'})
-    }
-
-}
-
-export const updateProduct =async (req, res)  => {
-    const {name, category, image, description, price, countinStock} = req.body;
-
-    const product = await Product.findById(req.parmas.id)
+    product = await product.save();
 
     if (product) {
-        product.name =name
-        product.category =category
-        product.image =image
-        product.description =description
-        product.price =price
-        product.countinStock =countinStock
-        
+      res.status(201).json(product);
+    } else {
+      res.status(500).json({ message: "Product Not Created" });
     }
-    else{
-        res.status(404).json({message: 'Product Not found'})
-    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
 
-    const updatedProduct = await product.save();
+export const updateProduct = async (req, res) => {
+  const { name, category, image, description, price, countinStock } = req.body;
 
-    if (updatedProduct) {
-        res.status(201).json(updatedProduct)
-        
-    }
-    
+  const product = await Product.findById(req.parmas.id);
 
+  if (product) {
+    product.name = name;
+    product.category = category;
+    product.image = image;
+    product.description = description;
+    product.price = price;
+    product.countinStock = countinStock;
+  } else {
+    res.status(404).json({ message: "Product Not found" });
+  }
 
-}
+  const updatedProduct = await product.save();
 
+  if (updatedProduct) {
+    res.status(201).json(updatedProduct);
+  }
+};
 
-
-export const deleteProduct = async (req, res)=>{
-
-    const product = await Product.findByIdAndDelete(req.parmas.id)
-    if (product) {
-        res.status(200).json({message:'Product deleted'})
-        
-    }
-}
+export const deleteProduct = async (req, res) => {
+  const product = await Product.findByIdAndDelete(req.parmas.id);
+  if (product) {
+    res.status(200).json({ message: "Product deleted" });
+  }
+};
