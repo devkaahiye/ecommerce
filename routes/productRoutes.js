4c91ddc1d02c1d02386afe9f61dea92c62b59751
unknown <kaah6978@gmail.com>
2023-05-15 00:27:09 +0300

    test

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { createProduct, deleteProduct, updateProduct } from '../controllers/productController.js'
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/productController.js'
 
 const router = express.Router()
 
 
-router.route('/').post(createProduct)
+router.route('/').post(createProduct).get(getProducts)
 router.route('/:id').put(updateProduct).delete(deleteProduct)
 
 export default router
