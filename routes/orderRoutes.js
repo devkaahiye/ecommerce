99701ec9ff156073d034dc789797744842074550
unknown <kaah6978@gmail.com>
2023-07-30 22:59:36 +0300

    test

--- routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { addOrderItems } from '../controllers/ordersController.js'
-
-const router = express.Router()
-
-router.route('/').post(addOrderItems)
-
-export default router
